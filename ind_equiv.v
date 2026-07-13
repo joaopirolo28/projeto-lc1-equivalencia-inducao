8e7fe3162384679fb4ad26e80ae8e157c1f78075
Arthur Bispo <64386652+arthurxsz@users.noreply.github.com>
2026-07-13 18:57:23 -0300

    Provando PIM <-> PIF

--- ind_equiv.v
+++ ind_equiv.v
@@ -2,6 +2,7 @@
 
 (* begin hide *)
 Require Import Arith.
+Require Import Classical.
 (* end hide *)
 
 (** Seja [P] uma propriedade sobre os números naturais. O Princípio da Indução Matemática (PIM) pode ser enunciado da seguinte forma:  *)
@@ -18,6 +19,39 @@ Definition PIF :=
   forall Q: nat -> Prop,
     (forall k, (forall m, m<k -> Q m) -> Q k) ->
     forall n, Q n.
+
+
+(** Dado um predicado [P] sobre naturais, se existe um natural [n] que satisfaz a propriedade [P], então existe um [m] que é o menor natural que satisfaz a propriedade [P]. Esta propriedade é conhecida como o Princípio da Boa Ordenação (PBO): *)
+Definition PBO := forall P : nat -> Prop,
+  (exists n : nat, P n) ->
+  exists m : nat, P m /\ forall x : nat, x < m -> ~ P x.
+
+(** Prove que estes princípios são equivalentes: *)
+
+Theorem PIM_equiv_PIF: PIM <-> PIF.
+Proof.
+  split.
+  - intro hweak.
+    intros P hP.
+    enough (H : forall n m, m < n -> P m).
+    -- intro n. apply (H (S n) n). apply le_n.
+    -- apply (hweak (fun n => forall m, m < n -> P m)).
+      + intros m hm. inversion hm.
+      + intros k hk m hm.
+        apply le_S_n in hm.
+        apply Nat.lt_eq_cases in hm.
+        destruct hm.
+        ++ apply hk. assumption.
+        ++ subst m. apply hP. assumption.
+  - intros hstrong P hbase hsucc.
+    apply hstrong.
+    intros k hk.
+    destruct k.
+    -- exact hbase.
+    -- apply hsucc.
+       apply hk.
+       apply le_n.
+Qed.
 
 
 (** Dado um predicado [P] sobre naturais, se existe um natural [n] que satisfaz a propriedade [P], então existe um [m] que é o menor natural que satisfaz a propriedade [P]. Esta propriedade é conhecida como o Princípio da Boa Ordenação (PBO): *)
